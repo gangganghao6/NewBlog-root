204799398bd659d551c79a9fbcbe789146fcbc44
dongyu <alpha_dong@yunquna.com>
2023-12-11 18:08:38 +0800

    feat：调整env

--- src/requests/request.ts
+++ src/requests/request.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 const url: string = import.meta.env.VITE_PUBLIC_URL
 const port: number = import.meta.env.VITE_SERVER_PORT
 export default axios.create({
-  baseURL: `${url}:${port}/api`,
+  baseURL: import.meta.env.DEV === true ? '/api' : `${url}:${port}/api`,
   timeout: 5000,
   withCredentials: true
 })
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteCompression from 'vite-plugin-compression'
+import fs from 'fs'
 import dotenv from 'dotenv'
 
-dotenv.config()
+dotenv.config({
+  path: fs.existsSync('.env.dev.local') ? '.env.dev.local' : '.env'
+})
+console.log(`${process.env.VITE_PUBLIC_URL}:${process.env.VITE_SERVER_PORT}`);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -17,14 +22,21 @@ export default defineConfig({
     })
   ],
   server: {
-    port: parseInt(process.env.VITE_PORT)
+    port: parseInt(process.env.VITE_PORT),
     // https: {
     //   cert: fs.readFileSync(join(localConfig.projectPath, 'keys/cert.crt')),
     //   key: fs.readFileSync(join(localConfig.projectPath, 'keys/cert.key'))
     // }
+    proxy: {
+      "/api": {
+        target: `http://${process.env.VITE_PUBLIC_URL}:${process.env.VITE_SERVER_PORT}`, // 目标地址
+        changeOrigin: true,            // 是否换源
+      },
+    },
   },
   build: {
     sourcemap: true,
     target: ['es2015', 'chrome58', 'firefox58', 'safari11']
-  }
+  },
+  base:'./'
 })
